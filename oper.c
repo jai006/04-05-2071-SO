9c7b452f237d4e42d90848ebf07bda984cdff11b
jai006 <jose.chinchilla@correounivalle.edu.co>
2017-04-05 16:21:05 -0500

    segunda clase programacion

---
+++ oper.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+void suma(int* a, int b);
+	*a = 5;
+	b = 12;
+	*a = *a + b;
+	
---
+++ oper.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+void suma(int* a, int b);
+	
+void resta(int* a, int b);
+	
+void sumav(int *a, int *b, int n);
+
+void restav(int *a, int *b, int n);
